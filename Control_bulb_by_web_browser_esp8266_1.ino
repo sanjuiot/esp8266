10f4de896a56d2b3fa52ed521e1144804b351bfd
sanjuiot <sanjeevsharmaengg@gmail.com>
2016-04-04 16:10:54 +0530

    Controlling electric appliance( bulb) through web browser on ESP8266-01
    Controlling electric appliance( bulb) through web browser on ESP8266-01

---
+++ Control_bulb_by_web_browser_esp8266_1.ino
@@ -0,0 +1,62 @@
+// Bulb control using the ESP8266-01 wifi 
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <aREST.h>
+#include <aREST_UI.h>
+
+// Create aREST instance
+aREST_UI rest = aREST_UI();
+
+// WiFi parameters
+const char* ssid = "MBLAZE-DF800i-B918";//Write the wifi name which is used as a transmiter,change your wifi name and password 
+const char* password = "88558855";//oasswird of thst wifi
+
+// The port to listen for incoming TCP connections 
+#define LISTEN_PORT           80
+// Create an instance of the 
+WiFiServer server(LISTEN_PORT);
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(115200);
+  
+  // Create UI
+  rest.title("Relay Control");
+  rest.button(2); /*2 is GPIO PIN 2 on ESP-01 where electric appliance is connected */ 
+    
+  // Give name and ID to device
+  rest.set_id("1");
+  rest.set_name("esp8266");
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+  
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+ 
+}
